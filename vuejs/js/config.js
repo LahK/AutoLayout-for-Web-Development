2f897626bede3fc5ee633351825b8503923cc7c0
Kang Cheng <me@chengkang.pw>
2017-03-17 12:35:14 +0000

    fix issue #19
    1. 编辑状态禁止当前工具栏拖动
    2. 使用 HTMLBeautify、JSFormat 和 CSSFormat 整理文件

--- vuejs/js/config.js
+++ vuejs/js/config.js
@@ -26,9 +26,7 @@ var ALConfig = {
 				'Bd-Radius': 'border-radius',
 				'Bg-Color': 'background-color'
 			},
-			data: {
-
-			}
+			data: {}
 		},
 		'AL-Label': {
 			style: {
@@ -81,38 +79,36 @@ var ALConfig = {
 				'Image-Pos': 'background-position',
 				'Image-Size': 'background-size'
 			},
-			data: {
-
-			}
+			data: {}
 		},
 	},
 	// Constraint-inspector Configs
 	constraints: {
 		single: {
 			Size: {
-				'Width':'c-width',
-				'Height':'c-height'
+				'Width': 'c-width',
+				'Height': 'c-height'
 			},
 			Spacing: {
-				'Leading Space':'c-leading-space',
-				'Top Space':'c-top-space',
-				'Trailing Space':'c-trailing-space',
-				'Bottom Space':'c-bottom-space'
+				'Leading Space': 'c-leading-space',
+				'Top Space': 'c-top-space',
+				'Trailing Space': 'c-trailing-space',
+				'Bottom Space': 'c-bottom-space'
 			},
 			Align: {
-				'Herizontally in Screen':'c-herizontally-in-box',
-				'Vertically in Screen':'c-vertically-in-box'
+				'Herizontally in Screen': 'c-herizontally-in-box',
+				'Vertically in Screen': 'c-vertically-in-box'
 			}
 		},
 		multi: {
-			'Equal Width':'c-equal-width',
-			'Equal Height':'c-equal-height',
-			'Align Leading Edges':'c-align-leading-edges',
-			'Align Top Edges':'c-align-top-edges',
-			'Align Trailing Edges':'c-align-traling-edges',
-			'Align Botton Edges':'c-align-bottom-edges',
-			'Horizontally Align':'c-horizontally-align',
-			'Vertically Align':'c-vertically-align',
+			'Equal Width': 'c-equal-width',
+			'Equal Height': 'c-equal-height',
+			'Align Leading Edges': 'c-align-leading-edges',
+			'Align Top Edges': 'c-align-top-edges',
+			'Align Trailing Edges': 'c-align-traling-edges',
+			'Align Botton Edges': 'c-align-bottom-edges',
+			'Horizontally Align': 'c-horizontally-align',
+			'Vertically Align': 'c-vertically-align',
 		}
 	}
 };
